7b493c47bbdf39579037313f8f1c75a05f8ac990
adamlogan17 <adamlogan17@users.noreply.github.com>
2023-07-29 20:35:39 +0100

    started conversion to react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h1>Hello!</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App

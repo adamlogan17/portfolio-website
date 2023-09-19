d508bcaffbb3250f8cb790a2d56d281d3b5c6c51
Adam Logan <adamlogan42@gmail.com>
2023-09-19 19:27:54 +0100

    initial tilt card experimentation

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -3,31 +3,35 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { Tilt } from 'react-tilt'
+
 
 export default function ProjectCard(props:ProjectCard) {
   return (
-    <Card sx={{ width: '300px', height: '490px', padding:'0px', margin:'10px' }}>
-      <CardActionArea href={props.projectUrl} target="_blank" sx={{
-          textDecoration:'none', 
-          height:'inherit',
-          '&:hover': {
-            color: "text.primary"
-          }}}>
-        <CardMedia
-          component="img"
-          height="300px"
-          image={props.img.src}
-          alt={props.img.alt}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.text}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Tilt style={{ width: '300px', height: '490px', margin:'20px', backgroundColor:'aqua' }}>
+      <Card style={{ width: '300px', height: '490px'}}>
+        <CardActionArea href={props.projectUrl} target="_blank" sx={{
+            textDecoration:'none', 
+            height:'inherit',
+            '&:hover': {
+              color: "text.primary"
+            }}}>
+          <CardMedia
+            component="img"
+            height="300px"
+            image={props.img.src}
+            alt={props.img.alt}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {props.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {props.text}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Tilt>
   );
 }
